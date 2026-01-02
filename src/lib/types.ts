2fc6a40e1b3c08d4702f85a444e4db5fc706e2c0
ybasviel <67199957+ybasviel@users.noreply.github.com>
2026-01-03 07:20:48 +0900

    足りてなかった定義を追加

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,17 +1,21 @@
 import type { MicroCMSImage, MicroCMSDate, MicroCMSContentId } from 'microcms-js-sdk';
 
 export type Work = {
+	id: string;
 	title: string;
 	description: string;
 	content: string;
 	thumbnail?: MicroCMSImage;
+	publishedAt: string;
 } & MicroCMSContentId &
 	MicroCMSDate;
 
 export type Blog = {
+	id: string;
 	title: string;
 	content: string;
 	thumbnail?: MicroCMSImage;
+	publishedAt: string;
 } & MicroCMSContentId &
 	MicroCMSDate;
 
