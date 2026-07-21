a824a1e5ba74de507588314cae52189ee0ce06bd
りんりん <67199957+ybasviel@users.noreply.github.com>
2026-07-21 23:25:48 +0900

    Revert "Revert "microcmsの画像ファイルをdlしてgithub pagesでホストする""

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { localizeImages } from './vite-plugin/localize-images';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({ plugins: [tailwindcss(), sveltekit(), localizeImages()] });
